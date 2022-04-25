f030e4068b69bb4db6da89e9e0602164a5c9a9a0
joaovitorribeiroc1@gmail.com <joaovitorribeiroc1@gmail.com>
2022-04-25 14:02:36 -0300

    navBar

--- src/navBar/navBar.js
+++ src/navBar/navBar.js
@@ -8,13 +8,10 @@ btnDrawer.addEventListener('click', (e) => {
         drawer.innerHTML = `
         <div class=''>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item list-group-item-dark"><i class="bi bi-person"></i>Minha conta</li>
-                <li class="list-group-item list-group-item-dark"><i class="bi bi-bag-heart"></i>Meus pedidos</li>
-                <li class="list-group-item list-group-item-dark"><i class="bi bi-basket2"></i>Promoções</li>
-                <li class="list-group-item list-group-item-dark"><i class="bi bi-headset"></i>Atendimento</li>
-                <li class="list-group-item list-group-item-dark"> <i class="bi bi-box-arrow-in-right"></i><botton>Log-in</botton> </li>
-                <li class="list-group-item list-group-item-dark"><i class="bi bi-person-plus"></i> <botton>Register</botton> </li>
-               
+                <li class="list-group-item list-group-item-dark">First item</li>
+                <li class="list-group-item list-group-item-dark">Second item</li>
+                <li class="list-group-item list-group-item-dark">Third item</li>
+                <li class="list-group-item list-group-item-dark"><button class='btn'>LOGIN</button></li>
             </ul>
         </div>`
         statusDrawer = 'open'
